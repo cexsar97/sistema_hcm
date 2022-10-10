07cef163620f1b4916e5793b31047fe4a047de59
cexsar97 <cesaraugustovargas97@gmail.com>
2022-10-10 10:19:42 -0400

    Agregando proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('maestra')
+@section("titulo", "Inicio")
+@section('contenido')
+<style type="text/css">
+        .card {background-color: #A9CAC7 }
+</style>
+    <div class="col-12 text-center">
+        <h1>Bienvenido, {{Auth::user()->name}}</h1>
+    </div>
+    @foreach([
+        [ "clientes", "prestamos", "aportes", "usuarios"]
+    ] as $modulos)
+    
+    <!--
+    ["productos", "ventas", "vender", "clientes"],
+    ["usuarios", "acerca_de", "soporte", "prestamos"]
+    ] as $modulos) -->
+        <div class="col-12 pb-2">
+            <div class="row">
+                @foreach($modulos as $modulo)
+                    <div class="col-12 col-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src="{{url("/img/$modulo.png")}}">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {{$modulo === "acerca_de" ? "Acerca de" : ucwords($modulo)}}
+                                </h5>
+                                <a href="{{route("$modulo.index")}}" class="btn btn-success">
+                                    Ir a&nbsp;{{$modulo === "acerca_de" ? "Acerca de" : ucwords($modulo)}}
+                                    <i class="fa fa-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    @endforeach
+@endsection
